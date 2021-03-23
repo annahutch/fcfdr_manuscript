211f0d323d038b091fb86db6d7b511699a16ff71
Anna Hutchinson <42926930+annahutch@users.noreply.github.com>
2021-03-23 16:26:05 +0000

    Add files via upload

--- GenoCanyon_analysis/code.R
+++ GenoCanyon_analysis/code.R
@@ -10,6 +10,7 @@ library(hexbin)
 library(bigsplines)
 library(locfdr)
 library(fcfdr)
+library(IHW)
 
 df <- readRDS("asthma_p.RDS") # data.frame of rsID (SNPID) and asthma GWAS p-values (european_ancestry_pval_rand) for 1968651 SNPs in asthma analysis
 ind_snps <- readRDS("ind_snps.RDS") # vector of rsIDs for the independent subset of SNPs
@@ -19,7 +20,14 @@ orig_p <- df$european_ancestry_pval_rand
 q_tmp <- readRDS("GenoCanyon_Prediction.RDS") # data.frame of chr (V1), pos (V2) and GenoCanyon score (V3) for 1968651 SNPs in asthma analysis
 q <- q_tmp$V3
 
-# run flexible cFDR
-res <- flexible_cfdr(p = orig_p, q, indep_index)
+fdr_thr <- 0.000148249 # FDR threshold corresponding to conventional genome-wide significance p-value threshold
 
-saveRDS(res, "GC_func_cFDR_res.RDS")
+# run IHW
+ihw_res <- ihw(orig_p, q, alpha = fdr_thr)
+
+# run flexible cFDR
+cfdr_res <- flexible_cfdr(p = orig_p, q, indep_index)
+
+saveRDS(ihw_res, "GC_IHW_res.RDS")
+saveRDS(cfdr_res, "GC_func_cFDR_res.RDS")
+
